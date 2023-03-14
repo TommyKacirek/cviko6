ca2e44bfb5200437fc4a2b0f1e49cd260530b4fa
TommyKacirek <101295400+TommyKacirek@users.noreply.github.com>
2023-03-14 13:12:22 +0100

    Update Graph.java

--- src/main/java/org/example/Graph.java
+++ src/main/java/org/example/Graph.java
@@ -1,39 +1,44 @@
-package org.example;
+
+
+
 import java.util.ArrayList;
 import java.util.List;
 
-public Graph() {
-    this.vertices = new ArrayList<>();
-    this.edges = new ArrayList<>();
-}
+public class Graph {
+    private List<Vertex> vertices;
+    private List<Edge> edges;
 
-public Vertex addVertex(String name) {
-    Vertex vertex = new Vertex(name);
-    vertices.add(vertex);
-    return vertex;
-}
-
-public Vertex getVertexByName(String name) {
-    for (Vertex vertex : vertices) {
-        if (vertex.getName().equals(name)) {
-            return vertex;
-        }
+    public Graph() {
+        this.vertices = new ArrayList<>();
+        this.edges = new ArrayList<>();
     }
-    return null;
-}
 
-public void addEdge(Vertex source, Vertex destination, int weight, String packagePath) {
-    Edge edge = new Edge(source, destination, weight, packagePath);
-    edges.add(edge);
-}
-
-public int getCost(Vertex source, Vertex destination, String packagePath) {
-    for (Edge edge : edges) {
-        if (edge.getSource().equals(source) && edge.getDestination().equals(destination) && edge.getPackagePath().startsWith(packagePath)) {
-            return edge.getPrice();
-        }
+    public Vertex addVertex(String name) {
+        Vertex vertex = new Vertex(name);
+        vertices.add(vertex);
+        return vertex;
     }
-    return -1;
+
+    public Vertex getVertexByName(String name) {
+        for (Vertex vertex : vertices) {
+            if (vertex.getName().equals(name)) {
+                return vertex;
+            }
+        }
+        return null;
+    }
+
+    public void addEdge(Vertex source, Vertex destination, int weight, String packagePath) {
+        Edge edge = new Edge(source, destination, weight, packagePath);
+        edges.add(edge);
+    }
+
+    public int getCost(Vertex source, Vertex destination, String packagePath) {
+        for (Edge edge : edges) {
+            if (edge.getSource().equals(source) && edge.getDestination().equals(destination) && edge.getPackagePath().startsWith(packagePath)) {
+                return edge.getPrice();
+            }
+        }
+        return -1;
+    }
 }
-
-
