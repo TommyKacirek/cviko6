607f65966a8930382e2e071e8203ffd3fbcab93b
TommyKacirek <101295400+TommyKacirek@users.noreply.github.com>
2023-03-14 14:40:31 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,4 @@
 package org.example;
-
 public class Main {
     public static void main(String[] args) {
         // vytvoření grafu
@@ -10,19 +9,30 @@ public class Main {
         Vertex b = graph.addVertex("Router 2");
         Vertex c = graph.addVertex("Router 3");
         Vertex d = graph.addVertex("Router 4");
+        Vertex e = graph.addVertex("Router 5");
 
         // přidání hran s cenami
-        graph.addEdge(a, b, 5);
-        graph.addEdge(a, c, 3);
-        graph.addEdge(b, d, 2);
-        graph.addEdge(c, d, 4);
+        graph.addEdge(a, b, 6);
+        graph.addEdge(b, c, 5);
+        graph.addEdge(c, d, 11);
+        graph.addEdge(d, e, 9);
+        
+        System.out.println(graph.toString());
+        
+        
 
         // nalezení ceny mezi vrcholy
-        int cost = graph.getCost(a, b);
-        System.out.println("Cena mezi router 1 a router 2: " + cost);
+        graph.getCost(a, b);
+        graph.getCost(b, c);
+        graph.getCost(c, d);
+        graph.getCost(d, e);
+        System.out.println("Cena mezi router 1 a router 5: " + graph.getValue());
+       
+     
+        
 
         // nalezení vrcholu podle jména
-        Vertex vertex = graph.getVertexByName("Router 4");
+        Vertex vertex = graph.getVertexByName("Router 5");
         System.out.println("Hledani routeru : " + vertex);
     }
 }
